64e781c6e9f9cfffaeb5861d79ad5108ac23ac61
priyanshi689 <agarwalpriyanshi361@gmail.com>
2026-04-02 09:25:38 +0530

    Adding deque implemntation with linked list

---
+++ Deque/implementationWithLinkedList.java
@@ -0,0 +1,91 @@
+
+
+class Node{
+	    int data;
+	    Node prev;
+	    Node next;
+
+	    Node(int data) {
+	        this.data = data;
+	        this.next = null;
+	        this.prev = null;
+	    }
+	}
+class Deque{
+	Node rear;
+	Node front;
+	
+	void insertAtFront(int val) {
+		Node newnode = new Node(val);
+		
+		if(front == null) {
+			front = newnode;
+			rear = newnode;
+			return;
+		}
+		newnode.next = front;
+		front.prev = newnode;
+		front = newnode; 
+	}
+	void insertAtEnd(int val) {
+		Node newnode = new Node(val);
+		if(rear == null) {
+			front  = newnode;
+			rear = newnode;
+			return;
+		}
+		rear.next = newnode;
+	    newnode.prev = rear;
+	    rear = newnode;  
+	}
+	void display() {
+	    if (front == null) {
+	        System.out.println("Deque is empty");
+	        return;
+	    }
+
+	    Node temp = front;
+
+	    while (temp != null) {
+	        System.out.print(temp.data + " ");
+	        temp = temp.next;
+	    }
+
+	    System.out.println();
+	}
+	int deleteAtFront() {
+	    if (front == null) {
+	        return;
+	    }
+
+	    int val = front.data;
+	    front = front.next;
+
+	    if (front != null) {
+	        front.prev = null;
+	    } else {
+	        rear = null;
+	    }
+
+	    return val;
+	}
+	int deleteAtEnd() {
+	    if (rear == null) {
+	        throw new RuntimeException("Deque is empty");
+	    }
+
+	    int val = rear.data;
+
+	    // move rear backward
+	    rear = rear.prev;
+
+	    if (rear != null) {
+	        rear.next = null;
+	    } else {
+	        // deque became empty
+	        front = null;
+	    }
+
+	    return val;
+	}
+}
